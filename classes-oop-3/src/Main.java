c5bc7e724b9a1ab42f669ff4795a3d6a9e179d0a
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-07-22 22:54:30 +0100

    intro to interfaces

--- classes-oop-3/src/Main.java
+++ classes-oop-3/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        /*
         Circle circle = new Circle(9.2);
         Triangle triangle = new Triangle(4, 5);
         Rectangle rectangle = new Rectangle(5, 8);
@@ -12,5 +13,17 @@ public class Main {
         System.out.println(circle.area());
         System.out.println(triangle.area());
         System.out.println(rectangle.area());
+        */
+
+        Rabbit rabbit = new Rabbit();
+        rabbit.flee();
+
+        Hawk hawk = new Hawk();
+        hawk.hunt();
+
+        Fish fish = new Fish();
+        fish.flee();
+        fish.hunt();
+
     }
 }
