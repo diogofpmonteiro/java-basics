ae306c75ae058d98beb01b992a697711cfb9b190
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-07-22 18:22:02 +0100

    overloaded constructors

--- classes/src/Main.java
+++ classes/src/Main.java
@@ -23,6 +23,32 @@ public class Main {
         */
 
 
+        User user1 = new User("Diogo");
+        User user2 = new User("Pat", "pat@gmail.com");
+        User user3 = new User("Sand", "sand@mail.com", 23);
+        User user4 = new User();
 
+        System.out.println("User 1: ");
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+        System.out.println();
+
+        System.out.println("User 2: ");
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+        System.out.println();
+
+        System.out.println("User 3: ");
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+        System.out.println();
+
+        System.out.println("User 4: ");
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
     }
 }
